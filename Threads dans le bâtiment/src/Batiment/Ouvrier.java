7426363295ae8f49dc24a42606bfe9f02bc428f3
AGUIRI <rayyane.aguiri@etud.univ-jfc.fr>
2019-02-15 11:40:10 +0100

    TPTHREAD

---
+++ Threads dans le bâtiment/src/Batiment/Ouvrier.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Batiment;
+
+/**
+ *
+ * @author raguiri
+ */
+public class Ouvrier {
+
+    public static void main (String[]args){
+        Electricien E1= new Electricien();
+        Electricien E2= new Electricien();
+        Electricien E3= new Electricien();
+        Platrier P1= new Platrier();
+        Platrier P2= new Platrier();
+        E1.travailler();
+        E2.travailler();
+        E3.travailler();
+        P1.travailler();
+        P2.travailler();
+        System.out.println("Tout le monde a fini");
+    }
+}
---
+++ Threads dans le bâtiment/src/Batiment/Platrier.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Batiment;
+
+/**
+ *
+ * @author raguiri
+ */
+public class Platrier extends Ouvrier {
+    private String name;
+    private String profession;
+
+    public void travailler() {
+        for (int i=0;i<10;i++){
+            System.out.println("je suis"+getName()+"je travaille comme"+getProfession()+"et je commence à travailler");
+        }
+        System.out.println("je suis"+getName()+"je travaille comme"+getProfession()+"et j'ai fini mon travail");
+    }
+   
+
+    private String getName() {
+        return name;
+    }
+
+    private String getProfession() {
+        return profession;
+    }
+
+    
+    
+}
